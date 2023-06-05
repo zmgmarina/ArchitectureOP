23e6953e9fde04524cb5bd9eb200d5b8ad67a880
Marina <zmgmarina@mail.ru>
2023-06-05 11:26:54 +0500

    HW_Cat

---
+++ src/HW_Cat/Cat.java
@@ -0,0 +1,39 @@
+package HW_Cat;
+
+public class Cat {
+    private String name;
+    private int appetite;
+
+    private int satiety;
+
+    public Cat(String name, int appetite, int satiety) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = satiety;
+    }
+
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+//    public void setAppetite(int appetite) {
+//        this.appetite = appetite;
+//    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getSatiety() {
+        return satiety;
+    }
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
+
+}
+
+
+
---
+++ src/HW_Cat/Plate.java
@@ -0,0 +1,27 @@
+package HW_Cat;
+
+public class Plate {
+    private int food;
+
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+    public void addFood(){
+        setFood(getFood() + 10);
+    }
+
+
+
+    public int getFood() {
+        return food;
+    }
+    public void setFood(int food) {
+        this.food = food;
+    }
+}
