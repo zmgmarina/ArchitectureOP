f338c31ca09928e8ec0d159aef75dde088347548
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-10-01 01:12:12 +0500

    Update PhoneFunctionsSamsung.java

--- src/Architecture_op/HW_3/PhoneFunctionsSamsung.java
+++ src/Architecture_op/HW_3/PhoneFunctionsSamsung.java
@@ -1,5 +1,6 @@
 package Architecture_op.HW_3;
 
+// Интерфейс содержит методы, которые реализует в себе класс Samsung
 public interface PhoneFunctionsSamsung {
     void SendSMS();
     void SendEMail();
