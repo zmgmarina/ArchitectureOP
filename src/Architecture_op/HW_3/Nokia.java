e72c38394491491e9e0e10b89d7884884f4d042f
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-10-01 01:13:51 +0500

    Update Nokia.java

--- src/Architecture_op/HW_3/Nokia.java
+++ src/Architecture_op/HW_3/Nokia.java
@@ -5,23 +5,24 @@ package Architecture_op.HW_3;
 
 public class Nokia extends Phone implements PhoneFunctionsNokia{
 
+ // реализация метода
     @Override
     public void SendSMS() {
         System.out.println("The phone Nokia sends SMS");
 
     }
-
+ // реализация метода
     @Override
     public void Bluetooth() {
         System.out.println("The phone Nokia has the function Bluetooth");
 
     }
-
+ // реализация метода
     @Override
     public void PhotoShooting() {
         System.out.println("The phone Nokia has the function PhotoShooting");
     }
-
+ // реализация метода
     @Override
     void Call(){
         System.out.println("Nokia calling");
