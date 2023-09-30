c3d9fa0d256401b3052b1202fbc0f1b21e95d697
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-10-01 00:51:48 +0500

    Update Main.java

--- src/Architecture_op/HW_3/Main.java
+++ src/Architecture_op/HW_3/Main.java
@@ -1,10 +1,10 @@
 package Architecture_op.HW_3;
 
-// Создание экземляров классов Samsung Nokia и Sony с вызовом методов
+//Разбила методы по конкретным интерфейсам для каждой модели телефона, для того чтобы телефон выполнял только те функции 
 
 public class Main {
     public static void main(String[] args) {
-
+// Создание экземляров классов Samsung Nokia и Sony с вызовом методов
         Samsung samsung = new Samsung();
         samsung.Bluetooth();
         samsung.PhotoVideoShooting();
